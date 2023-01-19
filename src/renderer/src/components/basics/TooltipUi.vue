2b04fa3fa0f55dcdc2d9b8fb7d8d7a55b9fb6760
tillderoquefeuil <tillderoquefeuil@gmail.com>
2023-01-19 18:22:47 +0100

    adding tooltip

---
+++ src/renderer/src/components/basics/TooltipUi.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+
+interface Props {
+    value: string
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+    <div class="tooltip-slot">
+        <slot />
+
+        <div class="tooltip-popover">
+            <span>{{ value }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.tooltip-slot {
+    position: relative;
+}
+.tooltip-popover {
+    @apply opacity-0;
+    @apply absolute z-10 -top-8;
+    @apply bg-bg-3 text-content-2 text-sm py-2 px-1 rounded;
+    @apply transition-opacity duration-200 ease-in-out;
+}
+/* .tooltip-slot .tooltip-popover, */
+.tooltip-slot:hover .tooltip-popover {
+    @apply block opacity-100;
+}
+</style>
