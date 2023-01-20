d54130af34db8899adfcf720675cebd350b817f7
tillderoquefeuil <tillderoquefeuil@gmail.com>
2023-01-20 11:35:43 +0100

    cropped container step

--- src/renderer/src/views/setup/ContainerView.vue
+++ src/renderer/src/views/setup/ContainerView.vue
@@ -50,22 +50,21 @@ store.layout.footer.next.disable = false
                     Go to OBS and create an scene in which you will overlay all your camera sources on top of each other.<br>
                     Gabin will <span class="text-white font-bold">automatically</span> toggle their visibility.
                 </p>
-                <div class="flex justify-between items-stretch w-full">
-                    <div class="w-1/2 pr-2">
+                <div class="flex flex-wrap justify-between items-stretch">
+                    <div class="w-1/2 min-w-[18rem] min-h-[6rem] pr-2">
                         <img
                             class="w-full"
                             :src="ObsContainerImg"
                         >
                     </div>
-
-                    <div class="w-1/4 px-2 py-[3%]">
+                    <div class="w-1/4 min-w-[9rem] min-h-[6rem] px-2 py-[3%]">
                         <div class="obs-scene relative w-full h-full">
                             <div class="obs-container w-full h-full">
                                 <span>Container</span>
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/4 pl-2 py-[3%]">
+                    <div class="w-1/4 min-w-[9rem] min-h-[6rem] pl-2 py-[3%]">
                         <div class="obs-scene w-full h-full flex justify-start items-end">
                             <div class="obs-container w-1/2 h-1/2">
                                 <span>Container</span>
@@ -83,12 +82,14 @@ store.layout.footer.next.disable = false
                     If you need a split view with several simultaneous camera views you can create several Containers.
                 </p>
 
-                <div class="flex justify-between items-stretch w-full">
-                    <img
-                        class="w-1/2 pr-2"
-                        :src="ObsContainersImg"
-                    >
-                    <div class="w-1/4 px-2 py-[3%]">
+                <div class="flex flex-wrap justify-between items-stretch w-full">
+                    <div class="w-1/2 min-w-[18rem] min-h-[6rem] pr-2">
+                        <img
+                            class="w-full"
+                            :src="ObsContainersImg"
+                        >
+                    </div>
+                    <div class="w-1/4 min-w-[9rem] min-h-[6rem] px-2 py-[3%]">
                         <div class="obs-scene w-full h-full flex">
                             <div class="obs-container w-1/2 h-full">
                                 <span>Container A</span>
@@ -98,7 +99,7 @@ store.layout.footer.next.disable = false
                             </div>
                         </div>
                     </div>
-                    <div class="w-1/4 px-2 py-[3%]">
+                    <div class="w-1/4 min-w-[9rem] min-h-[6rem] pl-2 py-[3%]">
                         <div class="obs-scene relative w-full h-full flex flex-col justify-start items-start">
                             <div class="obs-container w-1/2 h-1/2">
                                 <span>Container A</span>
@@ -119,17 +120,17 @@ store.layout.footer.next.disable = false
                     You can now embed and place your Container scene in your main scenes.<br>
                     Create <span class="text-white font-bold">at least one main scene</span> before next step.
                 </p>
-                <div class="flex justify-between items-stretch w-full h-44">
+                <div class="flex flex-wrap justify-between items-center w-full">
                     <img
-                        class="h-full"
+                        class="h-full max-h-50 pr-4 pb-4"
                         :src="ObsNewSceneImg"
                     >
                     <img
-                        class="h-full"
+                        class="h-full max-h-50 pr-4 pb-4"
                         :src="ObsAddSceneImg"
                     >
                     <img
-                        class="h-full"
+                        class="h-full max-h-44 pr-4 pb-4"
                         :src="ObsScenesImg"
                     >
                 </div>
@@ -144,7 +145,7 @@ store.layout.footer.next.disable = false
     @apply w-full flex flex-wrap items-start my-5;
 }
 .obs-container-info > div {
-    @apply bg-bg-2 p-4 my-5 min-w-[35rem] w-full;
+    @apply bg-bg-2 p-4 my-5 w-full;
 }
 
 .obs-scene {
