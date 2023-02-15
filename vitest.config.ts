a1bafcce52180fdd7df6b581b29c02c21213bde8
till de roquefeuil <tillderoquefeuil@gmail.com>
2023-02-15 12:34:16 +0100

    remove useless files/folder

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    include: ['src/tests/**/*.spec.ts'],
-    testTimeout: 10_000,
-    hookTimeout: 10_000,
-  },
-})
