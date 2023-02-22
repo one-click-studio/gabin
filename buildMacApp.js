af80a686ba1ecbd8c7c6cae72d2c238ea84438a8
till de roquefeuil <tillderoquefeuil@gmail.com>
2023-02-22 17:59:08 +0100

    build for arm64 & x64 (mac)

---
+++ buildMacApp.js
@@ -0,0 +1,30 @@
+const embler = require('embler')
+
+const config = {
+    name: "gabin",
+    author: "OneClickStudio",
+    version: "0.2.0",
+    embler: {
+        realName: "Gabin-",
+        appId: "com.oneclickstudio.gabin",
+        mac: {
+            binary: "./dist/gabin-",
+            icon: "./src/resources/icons/icon.png",
+            formats: ["app"],
+            category: "public.app-category.video"
+        }
+    }
+}
+
+const main = async () => {
+    const arch = process.argv[2]
+    if (!arch) return console.log("Please specify architecture (arm64 or x64)")
+
+    config.embler.realName += arch
+    config.embler.mac.binary += arch
+
+    // BUILD APP
+    await embler.build(config)
+}
+
+main()
