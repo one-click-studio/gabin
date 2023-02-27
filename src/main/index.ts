712482426f143c067c4e2c30ed5dfdce7b4ca83a
till de roquefeuil <tillderoquefeuil@gmail.com>
2023-02-27 12:10:23 +0100

    add flags & env var | fix ws with baseUrl

--- src/main/index.ts
+++ src/main/index.ts
@@ -13,8 +13,43 @@ import { ProfileSetup } from './modules/setup'
 import db from './utils/db'
 import { openUrl } from './utils/utils'
 import { Profile, Connection, ObsSource, MicId } from '../types/protocol'
+const PackageJson = require('../../package.json')
 
 dotenv.config()
+
+// get args
+const args = process.argv.slice(2)
+
+if (args.includes('--help') || args.includes('-h')) {
+  console.log(`
+  Usage: gabin [options]
+  Options:
+    -h, --help      Display this message
+    -d, --debug     Enable debug mode
+    -v, --version   Display version
+    --no-auto-open  Disable auto open
+
+  Environment variables:
+    GABIN_HOST          Hostname to use (default: localhost)
+    GABIN_PORT          Port to use (default: 1510)
+    GABIN_BASE_URL      Base url to use (default: /)
+    GABIN_LOGS_FOLDER   Folder to store logs (default: $appdata/gabin/gabin.log)
+    GABIN_CONFIG_FOLDER Folder to store config (default: $appdata/gabin/database.json)
+  `)
+  process.exit(0)
+}
+
+if (args.includes('--debug') || args.includes('-d')) {
+  process.env.DEBUG = 'true'
+}
+
+if (args.includes('--version') || args.includes('-v')) {
+  console.log('v' + PackageJson.version)
+  process.exit(0)
+}
+
+const AUTO_OPEN = !args.includes('--no-auto-open')
+const GABIN_BASE_URL = process.env.GABIN_BASE_URL || '/'
 
 const HOST = process.env.GABIN_HOST || 'localhost'
 const PORT = process.env.GABIN_PORT || 1510
@@ -153,17 +188,34 @@ function createTray(): Systray {
 
 async function main() {
 
+  console.log('\nbaseUrl', GABIN_BASE_URL)
   const serve = serveStatic(path.join(__dirname, '../render/dist'))
   const server = http.createServer((req, res) => {
-    serve(req, res, finaleHandler(req, res))
+    if (GABIN_BASE_URL !== '/') {
+      if (req.url?.substring(0, GABIN_BASE_URL.length) !== GABIN_BASE_URL) {
+        res.writeHead(404)
+        res.end()
+        return
+      }
+
+      req.url = req.url?.replace(GABIN_BASE_URL, '') || '/'
+    }
+
+    const s = serve(req, res, finaleHandler(req, res))
   })
 
-  const ioOptions: any = {}
+  const ioOptions: any = {
+    path: '/socket.io'
+  }
   if (process.env.GABIN_CLIENT_PORT) {
     ioOptions.cors = {
       origin: `http://localhost:${process.env.GABIN_CLIENT_PORT}`,
       methods: ["GET", "POST"]
     }
+  }
+
+  if (GABIN_BASE_URL !== '/') {
+    ioOptions.path = GABIN_BASE_URL + '/socket.io'
   }
 
   const io = new Server(server, ioOptions)
@@ -178,7 +230,8 @@ async function main() {
   handler(io)
 
   createTray()
-  openApp()
+
+  if (AUTO_OPEN) openApp()
 }
 
 main()
