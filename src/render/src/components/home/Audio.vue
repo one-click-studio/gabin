39c3bb09956b4fd7df21b7b0f3109d55c1bbfe0a
till de roquefeuil <tillderoquefeuil@gmail.com>
2023-03-28 13:02:48 +0200

    add connections summary

---
+++ src/render/src/components/home/Audio.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+
+import { store } from '@src/store/store'
+
+</script>
+
+<template>
+    <div class="w-full summary-audio">
+        <table class="table-auto">
+            <thead>
+                <tr>
+                    <th>Audio source</th>
+                    <th>Mic name</th>
+                    <th>Mic channel</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="(device, _i) in store.profiles.settings().mics">
+                    <template
+                        v-for="(mic, j) in device.micsName.filter((_m,index) => device.mics[index])"
+                        :key="`device-${_i}-mic-${j}`"
+                    >
+                        <tr>
+                            <td
+                                v-if="!j"
+                                :rowspan="device.mics.reduce((p,c) => p += c?1:0, 0)"
+                            >{{ device.name }}</td>
+                            <td>{{ mic }}</td>
+                            <td>Channel {{ j + 1 }}</td>
+                        </tr>
+                    </template>
+                </template>
+            </tbody>
+        </table>
+    </div>
+</template>
---
+++ src/render/src/components/home/Connections.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+
+import { ref } from 'vue'
+import { store } from '@src/store/store'
+
+import EyeIcon from '@src/components/icons/EyeIcon.vue'
+
+import type { 
+    Connection
+} from '../../../../types/protocol'
+
+type ConnectionType = 'tcp' | 'obs' | 'osc (server)' | 'osc (client)'
+
+const showPassword = ref<ConnectionType>()
+
+const getConnections = (): Map<ConnectionType, Connection> => {
+    const connections = store.profiles.connections()
+    const result: Map<ConnectionType, Connection> = new Map()
+
+    if (connections.tcp) {
+        result.set('tcp', connections.tcp)
+    }
+
+    if (connections.type === 'obs' && connections.obs) {
+        result.set('obs', connections.obs)
+    } else if (connections.type === 'osc' && connections.osc) {
+        result.set('osc (server)', connections.osc.server)
+        result.set('osc (client)', connections.osc.client)
+    }
+
+    return result
+}
+
+</script>
+
+<template>
+    <div class="w-full">
+        <table>
+            <thead>
+                <tr>
+                    <th>Connection type</th>
+                    <th>Host</th>
+                    <th>Port</th>
+                    <th v-if="store.profiles.connections().type === 'obs'" class="w-1/4">Password</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="[type, connection] in getConnections()">
+                    <tr>
+                        <td>{{ type }}</td>
+                        <td>{{ connection.ip.split(':')[0] }}</td>
+                        <td>{{ connection.ip.split(':')[1] }}</td>
+                        <td
+                            v-if="store.profiles.connections().type === 'obs'"
+                            class="flex justify-between items-center"
+                        >
+                            {{ showPassword === type? connection.password : connection.password?.replace(/./g, '*') }}
+                            <EyeIcon
+                                v-if="connection.password"
+                                class="w-4 h-4 cursor-pointer"
+                                @mousedown="showPassword = type"
+                                @mouseup="showPassword = undefined"
+                                @mouseleave="showPassword = undefined"
+                            />
+                        </td>
+                    </tr>
+                </template>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+</style>
