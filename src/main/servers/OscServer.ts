ff2f3c6ae7d108c693bd06525a7353370a63750b
till de roquefeuil <tillderoquefeuil@gmail.com>
2023-03-31 16:05:53 +0200

    OSC as permanent server

--- src/main/servers/OscServer.ts
+++ src/main/servers/OscServer.ts
@@ -1,60 +1,42 @@
 import OSC from 'osc-js'
+import { Subject } from 'rxjs'
 
 import { Server } from '../../main/servers/Server'
 
-import type {
-    Connection,
-    ConnectionsConfig,
-} from '../../types/protocol'
-
-import db from '../../main/utils/db'
+const CMD_TYPES = ['on', 'off', 'config', 'profile'] as const
+type CmdType = typeof CMD_TYPES[number]
 
 export class OscServer extends Server {
-    private oscConfig: {
-        client: Connection
-        server: Connection
-    } | undefined
+    command$: Subject<{type: CmdType, data: any}>
+
+    private config: {host: string, port: number}
     private server: OSC | undefined
 
-    constructor(fromProfile = true) {
+    constructor(host: string, port: number) {
         super('osc-server')
 
-        if (fromProfile) {
-            this.getConfigFromProfile()
-        }
+        this.command$ = new Subject()
+        this.config = { host, port }
     }
 
-    private getConfigFromProfile() {
-        const oscConfig = db.getSpecificAndDefault(['connections', 'osc'], true)
-        this.oscConfig = oscConfig.defaultValue
-    }
-
-    override listen(connections?: ConnectionsConfig['osc']) {
-        if (connections) {
-            this.oscConfig = connections
-        }
-
-        if (!this.oscConfig || !this.oscConfig.server || !this.oscConfig.client) return
+    override listen() {
         super.listen()
 
-        const serverIp = this.splitIp(this.oscConfig.server.ip)
-        const clientIp = this.splitIp(this.oscConfig.client.ip)
         const options = { 
             type: 'udp4',
-            open: { host: serverIp.host, port: serverIp.port },
-            send: { host: clientIp.host, port: clientIp.port }
+            open: { host: this.config.host, port: this.config.port },
         }
 
         this.server = new OSC({plugin: new OSC.DatagramPlugin(options)})
 
         this.server.on('open', () => {
             this.reachable$.next(true)
-            this.logger.info("connection was established")
+            this.logger.debug("connection was established")
         })
 
         this.server.on('close', () => {
             this.reachable$.next(false)
-            this.logger.info("connection was closed")
+            this.logger.debug("connection was closed")
         })
 
         this.server.on('error', (err: Error) => {
@@ -62,7 +44,45 @@ export class OscServer extends Server {
             this.logger.error("an error occurred", err.stack)
         })
 
+        this.addCommands()
+
         this.server.open()
+        this.logger.debug('listening on', this.config)
+    }
+
+    private addCommands() {
+        if (!this.server) return
+
+        // COMMANDS
+        this.server.on('/gabin/on', () => {
+            this.command('on', {})
+        })
+        this.server.on('/gabin/off', () => {
+            this.command('off', {})
+        })
+        this.server.on('/gabin/config', (message: any) => {
+            if (!message.args.length || [0,1].indexOf(message.args[0]) === -1) return
+            this.command('config', message.args[0], true)
+        })
+        this.server.on('/gabin/profile', (message: any) => {
+            if (!message.args.length || [0,1].indexOf(message.args[0]) === -1) return
+            this.command('profile', message.args[0])
+        })
+    }
+
+    private command(type: CmdType, data: any, toJson?: boolean) {
+        this.logger.debug('command', {type, data})
+        if (CMD_TYPES.indexOf(type) === -1) return
+
+        if (data && toJson) {
+            try {
+                data = JSON.stringify(data)
+            } catch (e) {
+                this.logger.error('Can\'t stringify data', data)
+                return
+            }
+        }
+        this.command$.next({ type, data })
     }
 
     override async clean() {
@@ -74,26 +94,21 @@ export class OscServer extends Server {
         super.clean()
     }
 
-    private splitIp(ip: string) {
-        const c = ip.split(':')
-        return {
-            host: c[0],
-            port: parseInt(c[1])
-        }
-    }
-
     on(path: string, callback: (message: any) => void) {
         if (!this.server) return
 
         this.server.on(path, callback)
     }
 
-    send(path: string) {
-        if (!this.server || !this.isReachable) return
+    send(path: string, port: number) {
+        if (!this.server || !this.isReachable || !this.config) return
 
         const message = new OSC.Message(path)
         this.logger.debug(`sending message: ${message.address}`)
-        this.server.send(message)
+        this.server.send(message, { port })
     }
 
+    getConfig(): {host: string, port: number} {
+        return this.config
+    }
 }
