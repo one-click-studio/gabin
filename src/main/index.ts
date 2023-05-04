f1cd5850d3512b03cd39e9e8e2c51223372267d4
tillderoquefeuil <tillderoquefeuil@gmail.com>
2023-05-04 18:10:02 +0200

    add silent option

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,9 +10,10 @@ if (args.includes('--help') || args.includes('-h')) {
     Usage: gabin [options]
     Options:
       -h, --help        Display this message
-      -d, --debug     Enable debug mode
-      -v, --version    Display version
-      --no-auto-open  Disable auto open
+      -d, --debug       Enable debug mode
+      -v, --version     Display version
+      -s, --silent      Hide logs (except errors & logs from plugins)
+      --no-auto-open    Disable auto open
 
     Environment variables:
       GABIN_HOST              Hostname to use (default: ${DEFAULT.HOST})
@@ -23,6 +24,10 @@ if (args.includes('--help') || args.includes('-h')) {
       GABIN_CONFIG_FOLDER     Folder to store config (default: $appdata/gabin/database.json)
     `)
     process.exit(0)
+}
+
+if (args.includes('--silent') || args.includes('-s')) {
+    process.env.SILENT = 'true'
 }
 
 if (args.includes('--debug') || args.includes('-d')) {
