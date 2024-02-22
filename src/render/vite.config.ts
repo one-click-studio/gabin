2470ce9257ce014735ea3d399c973ee6a1e56553
tillderoquefeuil <till-utils@proton.me>
2024-02-22 13:58:18 +0100

    rework tree structure

--- src/render/vite.config.ts
+++ src/render/vite.config.ts
@@ -5,6 +5,10 @@ import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, '../../build/main/render'),
+    emptyOutDir: true
+  },
   resolve: {
     alias: {
       '@src': resolve(__dirname, 'src'),
